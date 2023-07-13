258e58823030fb20966fc03c876887d6ed635e4f
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-07-13 16:03:46 +0300

    commit

--- components/MealItem.js
+++ components/MealItem.js
@@ -59,6 +59,7 @@ const styles = StyleSheet.create({
     title: {
         fontWeight: "bold",
         textAlign: "center",
+        color: "black",
         fontSize: 18,
         margin: 8,
     },
